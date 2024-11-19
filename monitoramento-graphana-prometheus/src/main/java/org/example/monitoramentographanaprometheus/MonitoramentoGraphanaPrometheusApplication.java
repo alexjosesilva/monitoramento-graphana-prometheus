4980bfc43277a06312abc7c3ee43f284e09040c7
ctl_ajose <ctl_ajose@uolinc.com>
2024-11-19 18:08:16 -0300

    Projeto Inicial

---
+++ monitoramento-graphana-prometheus/src/main/java/org/example/monitoramentographanaprometheus/MonitoramentoGraphanaPrometheusApplication.java
@@ -0,0 +1,13 @@
+package org.example.monitoramentographanaprometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoramentoGraphanaPrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitoramentoGraphanaPrometheusApplication.class, args);
+    }
+
+}
---
+++ monitoramento-graphana-prometheus/src/test/java/org/example/monitoramentographanaprometheus/MonitoramentoGraphanaPrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.monitoramentographanaprometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoGraphanaPrometheusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
